7cb712d7c9e5c189165599b75e02cca7b3cfa8fa
AliShadman95 <alishadman955@gmail.com>
2019-09-16 04:44:56 +0200

    second commit

--- portf/src/App.js
+++ portf/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
+import NavBar from "./Components/Navbar";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ portf/src/Components/Navbar.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav
+      className="navbar navbar-expand-lg site-navbar navbar-light bg-light sleep"
+      id="pb-navbar"
+    >
+      <div className="container">
+        <button
+          className="navbar-toggler collapsed"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarsExample09"
+          aria-controls="navbarsExample09"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="index.html">
+          Alias
+        </a>
+        <div
+          className="navbar-collapse justify-content-md-center collapse"
+          id="navbarsExample09"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" href="#section-home">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#section-about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#section-services">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#section-portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#section-resume">
+                Resume
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#section-blog">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#section-contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
