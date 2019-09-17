38bc185982dfe723bf68ae081faf01e8a5b4c18d
AliShadman95 <alishadman955@gmail.com>
2019-09-17 06:08:09 +0200

    fixed bugs

--- portf/src/App.js
+++ portf/src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
+import BackToTop from "../node_modules/react-back-to-top-button/build/index";
 import NavBar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import AboutMe from "./Components/AboutMe";
+
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import "./App.scss";
 
 export default class App extends Component {
   render() {
     // We pass a hidden prop to Navbar which can render className="hidden" if the prop is true
     return (
       <div className="App">
+        <BackToTop showAt={400} speed={1500} easing="easeInOutQuint">
+          <svg
+            style={{ fill: "#007bff" }}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path d="M6.101 359.293L25.9 379.092c4.686 4.686 12.284 4.686 16.971 0L224 198.393l181.13 180.698c4.686 4.686 12.284 4.686 16.971 0l19.799-19.799c4.686-4.686 4.686-12.284 0-16.971L232.485 132.908c-4.686-4.686-12.284-4.686-16.971 0L6.101 342.322c-4.687 4.687-4.687 12.285 0 16.971z" />
+          </svg>
+        </BackToTop>
         <NavBar />
         <Hero />
         <AboutMe />
--- portf/src/Components/Navbar.jsx
+++ portf/src/Components/Navbar.jsx
@@ -1,30 +1,10 @@
 import React, { Component } from "react";
 
 class NavBar extends Component {
-  state = { isHide: false };
-
-  hideBar = () => {
-    const { isHide } = this.state;
-
-    window.scrollY > this.prev
-      ? !isHide && this.setState({ isHide: true })
-      : isHide && this.setState({ isHide: false });
-
-    this.prev = window.scrollY;
-  };
-
-  componentDidMount() {
-    window.addEventListener("scroll", this.hideBar);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.hideBar);
-  }
   render() {
-    const classHide = this.state.isHide ? "hide" : "";
     return (
       <nav
-        className={`navbar navbar-expand-lg site-navbar navbar-light bg-light ${classHide}`}
+        className="navbar navbar-expand-lg site-navbar navbar-light bg-light"
         id="pb-navbar"
       >
         <div className="container">
@@ -46,35 +26,41 @@ class NavBar extends Component {
           >
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" href="#section-home">
+                <a className="nav-link active" href="#">
                   Home
                 </a>
               </li>
+
               <li className="nav-item">
                 <a className="nav-link" href="#section-about">
                   About
                 </a>
               </li>
+
               <li className="nav-item">
                 <a className="nav-link" href="#section-services">
                   Services
                 </a>
               </li>
+
               <li className="nav-item">
                 <a className="nav-link" href="#section-portfolio">
                   Portfolio
                 </a>
               </li>
+
               <li className="nav-item">
                 <a className="nav-link" href="#section-resume">
                   Resume
                 </a>
               </li>
+
               <li className="nav-item">
                 <a className="nav-link" href="#section-blog">
                   Blog
                 </a>
               </li>
+
               <li className="nav-item">
                 <a className="nav-link" href="#section-contact">
                   Contact
