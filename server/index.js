545464f5ec11688c4905458360f2a290f90ccc35
Ali <ali@Alis-MacBook.local>
2019-12-04 04:33:57 +0100

    cahged port

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 const cors = require("cors");
 var http = require("http");
 
