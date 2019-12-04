f7eb83747e7ba998fd08e4cb806a8784f7a4f716
Ali <ali@Alis-MacBook.local>
2019-12-04 05:58:23 +0100

    added build

---
+++ client/build/precache-manifest.3059530c52e80affc223e78d12e344d3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af379d252f2719624c0b0a624d862d00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9ad0c7522a10e1a3b42",
+    "url": "/static/css/2.8bf49afd.chunk.css"
+  },
+  {
+    "revision": "2849a2e5d68b4290b743",
+    "url": "/static/css/main.d5f53a87.chunk.css"
+  },
+  {
+    "revision": "d9ad0c7522a10e1a3b42",
+    "url": "/static/js/2.c578489c.chunk.js"
+  },
+  {
+    "revision": "2849a2e5d68b4290b743",
+    "url": "/static/js/main.0c544631.chunk.js"
+  },
+  {
+    "revision": "6bb5583fdf35008ebdb1",
+    "url": "/static/js/runtime~main.b722e538.js"
+  },
+  {
+    "revision": "ed6610fcb6bd43a3a11ca92c39d0bd65",
+    "url": "/static/media/bokeh_blue2_720.ed6610fc.mp4"
+  },
+  {
+    "revision": "30b560ed85cd04f3164f64d4b1cfe01c",
+    "url": "/static/media/chatscreen.30b560ed.png"
+  },
+  {
+    "revision": "7fbc7a34e95f31e0b3755f1b27d2cfc9",
+    "url": "/static/media/cutmypic.7fbc7a34.png"
+  },
+  {
+    "revision": "64c192f262d924858e759edba38a391d",
+    "url": "/static/media/poster.64c192f2.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a4d3233ec573d0a912a16eec77f188a5",
+    "url": "/static/media/workprog.a4d3233e.png"
+  }
+]);
