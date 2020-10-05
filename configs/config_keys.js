9167c59ac61e58dbd005e2abe529649e5bdcfe4b
Ali Shadman <ali.shadman@sourcesense.com>
2020-10-05 14:41:33 +0200

    minor fix

---
+++ configs/config_keys.js
@@ -0,0 +1,37 @@
+module.exports = {
+	"services": {
+		"jwt": {
+			"status": "disabled",
+			"token_secret": ""
+		},
+		"express": {
+			"status": "disabled",
+			"session_key": "",
+		},
+		"google": {
+			"status": "disabled",
+			"consumer_key": "",
+			"consumer_secret": ""
+		},
+		"facebook": {
+			"status": "disabled",
+			"consumer_key": "",
+			"consumer_secret": "",
+		},
+		"twitter": {
+			"status": "disabled",
+			"consumer_key": "",
+			"consumer_secret": ""
+		},
+		"instagram": {
+			"status": "disabled",
+			"consumer_key": "",
+			"consumer_secret": ""
+		},
+		"github": {
+			"status": "disabled",
+			"consumer_key": "",
+			"consumer_secret": ""
+		}
+	}
+};
